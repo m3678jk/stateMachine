d578831b8ba1d5c453252d72ce25705916510b53
m3678jk <94804170+m3678jk@users.noreply.github.com>
2022-04-09 17:56:01 +0200

    Update StateMachine.java

--- StateMachine.java
+++ StateMachine.java
@@ -1,12 +1,43 @@
-//import java.time.Clock;
+package stateMachine;
+
+import org.checkerframework.checker.units.qual.C;
+
+import java.sql.Time;
+import java.time.Clock;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StateMachine {
 
     private ChatSettings chatSettings;
-    //onCcySetting
+
+//    private List <StateMachineListener> listeners = new ArrayList<>();
+//
+//    void addListener(StateMachineListener stateMachineListener){
+//        listeners.add(stateMachineListener);
+//    }
+
     {
         chatSettings = new ChatSettings();
     }
+
+    public void setQtyOfSignAfterPoint(int number) {
+        chatSettings.setQuantityOfSignsAfterDot(number);
+    }
+
+    public void setBank(String name) {
+        chatSettings.setBank(name);
+    }
+
+    public void setNotificationTime(String text) {
+        if (text.equals("Notification off")) {
+            chatSettings.setDoNotify(false);
+        } else {
+            chatSettings.setNotifyTime(new Time(Integer.parseInt(text)));
+        }
+
+    }
+
 
     public void setCcy(String text) {
         //if pressed button "USD" and this setting was not active before, then switch on this function
@@ -33,8 +64,10 @@ public class StateMachine {
         }
 
     }
-    void save (){
+
+    void save() {
         System.out.println("data is saved - method should be done");
+        //TODO
     }
 
 
@@ -42,14 +75,21 @@ public class StateMachine {
 
 class ChatSettings {
 
-    //boolean isCurrencyNeeded;
-    //List<Boolean> bankNeeded;
     private boolean isUsdNeed;
     private boolean isEurNeed;
+    private String bank;
+    private int quantityOfSignsAfterDot;
+    private boolean doNotify;
+    private Time notifyTime;
 
-//    int quantityOfSignsAfterDot;
-//    boolean doNotify;
-//    Clock notifyTime;
+    public int getQuantityOfSignsAfterDot() {
+        return quantityOfSignsAfterDot;
+    }
+
+    public void setQuantityOfSignsAfterDot(int quantityOfSignsAfterDot) {
+        this.quantityOfSignsAfterDot = quantityOfSignsAfterDot;
+    }
+
 
     public boolean isUsdNeed() {
         return isUsdNeed;
@@ -66,8 +106,41 @@ class ChatSettings {
     public void setEurNeed(boolean eurNeed) {
         isEurNeed = eurNeed;
     }
-    //Public stateMachine.ChatSettings() пробігаючи по лісту валют, лісту банків
-    //ініціює currencyNeeded, bankNeeded
+
+    public void setBank(String bank) {
+        this.bank = bank;
+    }
+
+    public String getBank() {
+        return bank;
+    }
+
+    public void setDoNotify(boolean doNotify) {
+        this.doNotify = doNotify;
+    }
+
+    public boolean isDoNotify() {
+        return doNotify;
+    }
+
+    public void setNotifyTime(Time notifyTime) {
+        this.notifyTime = notifyTime;
+    }
+    
+    public Time getNotifyTime() {
+        return notifyTime;
+    }
+
+    //   it maybe will be needed for creating default settings, but for now it just disturbs to use above methods
+//    public ChatSettings(boolean isUsdNeed, boolean isEurNeed, String bank, int quantityOfSignsAfterDot, boolean doNotify, Clock notifyTime) {
+//        this.isUsdNeed = isUsdNeed;
+//        this.isEurNeed = isEurNeed;
+//        this.bank = bank;
+//        this.quantityOfSignsAfterDot = quantityOfSignsAfterDot;
+//        this.doNotify = doNotify;
+//        this.notifyTime = notifyTime;
+//    }
+    
 }
 
 
