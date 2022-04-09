174749b2896ab20809a8fd9942e71c42bac3b6cc
m3678jk <94804170+m3678jk@users.noreply.github.com>
2022-04-09 13:37:34 +0200

    Create ChatPlace.java

---
+++ ChatPlace.java
@@ -0,0 +1,6 @@
+public enum ChatPlace {
+    idle, onStart, onInfoOrSetting, onInfo, onQtyAndBankAndCcyAndTime, onCcySetting, onQtySetting, onBankSetting,
+    onTimeSetting,
+    onCurrencyChosen,
+    getOnTimeSetting
+}
