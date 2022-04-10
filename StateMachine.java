847f3ac9121c78931f4e2651b814293e9db001b2
m3678jk <94804170+m3678jk@users.noreply.github.com>
2022-04-10 13:31:48 +0200

    Update StateMachine.java

--- StateMachine.java
+++ StateMachine.java
@@ -1,10 +1,11 @@
-import java.sql.Time;
+package stateMachine;
 
+import lombok.Data;
+import java.sql.Time;
 
 public class StateMachine {
 
     private ChatSettings chatSettings = new ChatSettings();
-    
 
     public void setQtyOfSignAfterPoint(int number) {
         chatSettings.setQuantityOfSignsAfterDot(number);
@@ -18,7 +19,7 @@ public class StateMachine {
         if (text.equals("Notification off")) {
             chatSettings.setDoNotify(false);
         } else {
-            chatSettings.setNotifyTime(new Time(Integer.parseInt(text), 0,0));
+            chatSettings.setNotifyTime(new Time(Integer.parseInt(text), 0, 0));
         }
 
     }
@@ -49,12 +50,17 @@ public class StateMachine {
         }
 
     }
-
+    public String getInfo(){
+        String result = "";
+        //TODO there should be method which provide info from CurrencyServices based oh setting
+        //
+        return result;
+    }
     void save() {
-        System.out.println("data is saved - method should be done");
+        System.out.println("data is saved");
 
         System.out.println("stateMachine.toString() = " + toString());
-        //TODO our instance of StateMachine
+        //
         // should be saved in Map with <ChatId, StateMachine>
 
         // then separately using stateMachine setting look up for needed info in currency service
@@ -67,7 +73,7 @@ public class StateMachine {
                 '}';
     }
 }
-
+@Data
 class ChatSettings {
 
     private boolean isUsdNeed;
@@ -77,56 +83,7 @@ class ChatSettings {
     private boolean doNotify;
     private Time notifyTime;
 
-    public int getQuantityOfSignsAfterDot() {
-        return quantityOfSignsAfterDot;
-    }
-
-    public void setQuantityOfSignsAfterDot(int quantityOfSignsAfterDot) {
-        this.quantityOfSignsAfterDot = quantityOfSignsAfterDot;
-    }
-
-
-    public boolean isUsdNeed() {
-        return isUsdNeed;
-    }
-
-    public void setUsdNeed(boolean usdNeed) {
-        isUsdNeed = usdNeed;
-    }
-
-    public boolean isEurNeed() {
-        return isEurNeed;
-    }
-
-    public void setEurNeed(boolean eurNeed) {
-        isEurNeed = eurNeed;
-    }
-
-    public void setBank(String bank) {
-        this.bank = bank;
-    }
-
-    public String getBank() {
-        return bank;
-    }
-
-    public void setDoNotify(boolean doNotify) {
-        this.doNotify = doNotify;
-    }
-
-    public boolean isDoNotify() {
-        return doNotify;
-    }
-
-    public void setNotifyTime(Time notifyTime) {
-        this.notifyTime = notifyTime;
-    }
-
-    public Time getNotifyTime() {
-        return notifyTime;
-    }
-
-     //  default settings
+    //default data
     public ChatSettings() {
         this.isUsdNeed = true;
         this.isEurNeed = false;
@@ -134,7 +91,7 @@ class ChatSettings {
         this.quantityOfSignsAfterDot = 2;
         this.doNotify = true;
         //cannot find alternative
-        this.notifyTime = new Time(11, 0,0);
+        this.notifyTime = new Time(11, 0, 0);
     }
 
     @Override
