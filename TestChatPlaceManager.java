0c811e9f3e0a3d525176db32d98921ba35e3c93d
m3678jk <94804170+m3678jk@users.noreply.github.com>
2022-04-10 12:05:37 +0200

    Update TestChatPlaceManager.java

--- TestChatPlaceManager.java
+++ TestChatPlaceManager.java
@@ -3,6 +3,13 @@ import java.util.Scanner;
 public class TestChatPlaceManager {
     public static void main(String[] args) {
         ChatPlaceManager chatPlaceManager = new ChatPlaceManager();
+
+        System.out.println("Commands: Set setting,\n" +
+                " Ccy setting - USD and EUR\n" +
+                "Set qty - for now just 1 and 2 \n" +
+                "Set time - for now just 11 and 12, \n" +
+                "Ok\n" +
+                "Set bank - Mono, Privat, NBU");
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String text = scanner.nextLine();
